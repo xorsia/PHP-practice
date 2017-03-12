ce0d267a8d3f665aeb171b19954b846d506c927a
Aleksandr-Borodin <aleksandrov477@gmail.com>
2017-03-12 19:21:00 +0200

    Улучшена логика, добавлены комментарии

--- path1.php
+++ path1.php
@@ -1,6 +1,11 @@
 <?php
-  require __DIR__ . '\path-subsidiary.php';
+  // Подключаемый файл;
+  $p = 'path-subsidiary.php';
+  // Путь разбит на составные;
+  require __DIR__ . '\\' . $p;
+  // Текущая папка;
   $a1 = './';
+  // Путь для проверки DIRECTORY_SEPARATOR
   $a2 = 'C:/';
   $b = realpath($a2 . DIRECTORY_SEPARATOR);
   echo 'Проверка работы константы слешей ' . $b;

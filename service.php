896300326961dda7b0a75c23af9552c5141fa22c
Aleksandr-Borodin <aleksandrov477@gmail.com>
2017-03-13 14:51:44 +0200

    ini_set();

--- service.php
+++ service.php
@@ -1,6 +1,11 @@
 <?php
+  // Настройка показа ошибок;
+  ini_set('display_errors', '1');
+  ini_set('log-errors', '1');
+  $p = __DIR__ . '\er.log';
+  ini_set('error_log', $p);
   // Информация о сервере;
   // phpinfo();
   echo getcwd();    // как узнать текущую папку;
   echo "<br>" . 'Пять больше одного:' . "<br>";
-  echo assert(5>1);
+  echo asert(5>1);

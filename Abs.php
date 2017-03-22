f7f12bfa0e16aa3b9a406b718bd39265bc9faeaf
Aleksandr-Borodin <aleksandrov477@gmail.com>
2017-03-22 14:17:07 +0200

    spl_autoload_register

--- Abs.php
+++ Abs.php
@@ -1,7 +1,12 @@
 <?php
 // Простая автозагурзка классов;
-$p = __DIR__ . '\autoload.php';
+//$p = __DIR__ . '\autoload.php';
+//require_once $p;
+// Автозагрузка классво через класс (использование ооп);
+$p = __DIR__ . '\load.php';
 require_once $p;
+$loader = new Load();
+spl_autoload_register([$loader, 'loadclass']);
 // Проверка разницы поведения static и self;
 // Классы Fr являются идентичными De и наследниками абстрактного к. Abs пространства имен Abs;
 $a = new Fr();

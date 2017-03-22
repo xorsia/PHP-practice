2cf274691a4510a14ee41356fa92a9e1934a874e
Aleksandr-Borodin <aleksandrov477@gmail.com>
2017-03-22 13:53:34 +0200

    final p. f. в абстрактном классе

--- Classes/Abs/Abs.php
+++ Classes/Abs/Abs.php
@@ -2,14 +2,18 @@
 // Абстрактный клас пространства имен Abs;
 namespace Abs;
 abstract class Abs {
-    public $a = 'Подключен абстрактный класс';
-    public static $b = 1;
-    // self - принадлежит родителю, а не дочернему;
-    public function lio() {
-      echo "<br>" . self::$b++;
-    }
-    // static - принадлежит дочернему, а не родителю;
-    public function lio2() {
-      echo "<br>" . static::$b++;
-    }
+  public $a = 'Подключен абстрактный класс';
+  public static $b = 1;
+  // self - принадлежит родителю, а не дочернему;
+  public function lio() {
+    echo "<br>" . self::$b++;
+  }
+  // static - принадлежит дочернему, а не родителю;
+  public function lio2() {
+    echo "<br>" . static::$b++;
+  }
+  // ;
+  final public function endsh() {
+    echo "<br>" . 'Классы наследники не могут поменять эту ф-цию';
+  }
 }

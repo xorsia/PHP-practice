0af37eab7baefd6f0ce59843f885e3581f9754fa
Aleksandr-Borodin <aleksandrov477@gmail.com>
2017-03-22 13:54:49 +0200

    Убрана лишняя табуляция

--- Classes/De.php
+++ Classes/De.php
@@ -1,7 +1,7 @@
 <?php
 class De extends  Abs\Abs implements Nano {
-    public static $b = 1;
-    public function aust() {
-      echo "<br>" . 'Подключен интерфейс Нано';
-    }
+  public static $b = 1;
+  public function aust() {
+    echo "<br>" . 'Подключен интерфейс Нано';
+  }
 }

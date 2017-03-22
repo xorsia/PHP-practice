4604e638fe741f07e5867c6b8c97a7f04c59d773
Aleksandr-Borodin <aleksandrov477@gmail.com>
2017-03-22 13:55:55 +0200

    Попытка изменить final p. f. унаследованного абстрактного класса

--- Classes/Fr.php
+++ Classes/Fr.php
@@ -4,4 +4,8 @@ class Fr extends Abs\Abs {
   public function pa () {
     echo $this->a;
   }
+  // Нижестоящий код не может быть выполнен;
+  //public function endsh() {
+  //  echo 'Дочерняя ф-ция меняет метод';
+  //}
 }

853362b6ddae0a8ea75f99f8d1d0e9bdc93ca3fe
Aleksandr-Borodin <aleksandrov477@gmail.com>
2017-03-22 13:34:44 +0200

    Комментарии относительно пространства имен и позднего статического связывания

--- Classes/Abs/Abs.php
+++ Classes/Abs/Abs.php
@@ -1,12 +1,15 @@
 <?php
+// Абстрактный клас пространства имен Abs;
 namespace Abs;
 abstract class Abs {
-    public $a = 'Абстрактный класс';
+    public $a = 'Подключен абстрактный класс';
     public static $b = 1;
+    // self - принадлежит родителю, а не дочернему;
     public function lio() {
-        echo "<br>" . self::$b++;
+      echo "<br>" . self::$b++;
     }
+    // static - принадлежит дочернему, а не родителю;
     public function lio2() {
-        echo "<br>" . static::$b++;
+      echo "<br>" . static::$b++;
     }
 }

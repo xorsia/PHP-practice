b811f128eea8f248d832f9d7f9963da476b6f229
Aleksandr-Borodin <aleksandrov477@gmail.com>
2017-03-22 13:54:22 +0200

    Убраны лишние отступы

--- Classes/Avrelii.php
+++ Classes/Avrelii.php
@@ -1,20 +1,19 @@
 <?php
 class Avrelii extends Mark {
-    public function mr() {
-        //$this->yii(); не сработает, так как метод yii - private;
-        parent::hi();   // будет работать!
-        echo 'текст дочернего класса' . "<br>";
-    }
-    public function __toString()
-    {
-        return 'Это класс Avrelii';
-    }
-    const HELLO = 'Константа класса Avrelii';
-    public function ps() {
-        return self::HELLO;
-    }
-    public static $xb = 1;
-    public function wii() {
-        echo self::$xb++;
-    }
+  public function mr() {
+    //$this->yii(); не сработает, так как метод yii - private;
+    parent::hi();   // будет работать!
+    echo 'текст дочернего класса' . "<br>";
+  }
+  public function __toString() {
+    return 'Это класс Avrelii';
+  }
+  const HELLO = 'Константа класса Avrelii';
+  public function ps() {
+    return self::HELLO;
+  }
+  public static $xb = 1;
+  public function wii() {
+    echo self::$xb++;
+  }
 }

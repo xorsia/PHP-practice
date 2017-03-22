a37e111007843b76fc3948dc142fbb53bb672a52
Aleksandr-Borodin <aleksandrov477@gmail.com>
2017-03-22 14:31:11 +0200

    Класс с использованием интерфейса Iterator

---
+++ Classes/Myiterator.php
@@ -0,0 +1,22 @@
+<?php
+class Myiterator implements Iterator {
+  private $obj;
+  public function __construct() {
+    
+  }
+  public function current() {
+
+  }
+  public function key() {
+
+  }
+  public function next() {
+
+  }
+  public function rewind() {
+
+  }
+  public function valid() {
+
+  }
+}

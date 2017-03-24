51ecbfd3e19bfe14a72f691c53ec22d79f73de5b
Aleksandr-Borodin <aleksandrov477@gmail.com>
2017-03-24 23:03:44 +0200

    Закомментирован лишний, на данный момент, текст

--- Classes/Er.php
+++ Classes/Er.php
@@ -16,6 +16,6 @@ class Er extends Exception{
     }
   }
   public function hand3(\Exception $e) {
-    echo "<br>" . $e->getMessage();
+    //echo "<br>" . 'Поймано исключение класса ' . get_class($e) . ' Текст ошибки: ' . $e->getMessage();
   }
 }

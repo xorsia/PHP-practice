51ed12f2fe5390a012d68b77066f7ca7d5baa1d3
Aleksandr-Borodin <aleksandrov477@gmail.com>
2017-03-24 16:39:50 +0200

    Подготовлены и закомент. фатальная и нефатальная ошибки

--- exc.php
+++ exc.php
@@ -1,13 +1,17 @@
 <?php
 // Обработка ошибок, исключений;
 ini_set('display_errors', '1');
+error_reporting(E_ALL);
 // Автозагрузка классво через класс (использование ооп);
 $p = __DIR__ . '\load.php';
 require_once $p;
 $loader = new Load();
 spl_autoload_register([$loader, 'loadclass']);
-error_reporting(E_ALL);
 // Создание класса для работы с ошибками-исключениями;
 $p = new Er();
 $p->hand();
 $a = new Avrelii();
+// Выскочит нефатальная ошибка;
+//include 'D:\test.php';
+// Выскочит фатальная ошибка;
+//require 'D:\test.php';

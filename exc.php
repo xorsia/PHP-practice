469ba1d0ddf6825997cc98ce451e26f34e192d90
Aleksandr-Borodin <aleksandrov477@gmail.com>
2017-03-24 15:30:23 +0200

    Файл тестирования исключений и ошибок. Простая реализация исключений

---
+++ exc.php
@@ -0,0 +1,18 @@
+<?php
+// Обработка ошибок, исключений;
+ini_set('display_errors', '1');
+// Автозагрузка классво через класс (использование ооп);
+$p = __DIR__ . '\load.php';
+require_once $p;
+$loader = new Load();
+spl_autoload_register([$loader, 'loadclass']);
+error_reporting(E_ALL);
+// Создание класса для работы с ошибками-исключениями;
+$p = new Er();
+$p->hand();
+try {
+  $a = new Avrelii();
+}
+catch (Exception $e) {
+    echo $e->getFile();
+}

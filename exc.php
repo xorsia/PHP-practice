efc739f4f79fe24b670188bd96ce7d3710cdec75
Aleksandr-Borodin <aleksandrov477@gmail.com>
2017-03-24 23:04:49 +0200

    Ловля исключений определенных классов и создание соответствующих классов для передачи исключения в них.

--- exc.php
+++ exc.php
@@ -7,11 +7,26 @@ $p = __DIR__ . '\load.php';
 require_once $p;
 $loader = new Load();
 spl_autoload_register([$loader, 'loadclass']);
-// Создание класса для работы с ошибками-исключениями;
-$p = new Er();
-$p->hand();
-$a = new Avrelii();
-// Выскочит нефатальная ошибка;
+// Класс для примера, в нем выпадет исключение класса Er;
+try {
+  $a = new Avrelii();
+}
+catch(FileEr $e) {
+  $p2 = new FileEr();
+  $p2->hand();
+  throw new FileEr('Передадим в класс FileEr');
+}
+catch(DataEr $e) {
+  $p3 = new DataEr();
+  $p3->hand();
+  throw new DataEr('Передадим в класс DataEr');
+}
+catch(Exception $e) {
+  echo 'Исключения остальные';
+}
+$p1 = new Er();
+$p1->hand();
+//Выскочит нефатальная ошибка;
 //include 'D:\test.php';
 // Выскочит фатальная ошибка;
 //require 'D:\test.php';

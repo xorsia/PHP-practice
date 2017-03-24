f4e707f056648d9a9ba2c240ed9c7b9dddf3557e
Aleksandr-Borodin <aleksandrov477@gmail.com>
2017-03-24 16:37:12 +0200

    Класс Er является базовым классом для работы с исключениями и ошибками

--- Classes/Er.php
+++ Classes/Er.php
@@ -1,7 +1,19 @@
 <?php
-class Er {
+// Базовый класс работы с исключениями и ошибками;
+class Er extends Exception{
   public function hand() {
+    set_error_handler([$this, 'hand1']);
+    register_shutdown_function([$this, 'hand2']);
     set_exception_handler([$this, 'hand3']);
+  }
+  public function hand1($errno, $errstr, $errfile, $errline) {
+    echo "<br>" . 'Какая то нефатальная ошибка ';
+  }
+  public function hand2() {
+    if($error = error_get_last()) {
+      ob_get_clean();
+      echo "<br>" . 'Фатальная ошибка ';
+    }
   }
   public function hand3(\Exception $e) {
     echo "<br>" . $e->getMessage();

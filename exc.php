64bbb9d4d7f065c050d9f37f542849957c6bff9f
Aleksandr-Borodin <aleksandrov477@gmail.com>
2017-03-25 14:57:49 +0200

    2-х уровневый try-catch

--- exc.php
+++ exc.php
@@ -8,25 +8,33 @@ require_once $p;
 $loader = new Load();
 spl_autoload_register([$loader, 'loadclass']);
 // Класс для примера, в нем выпадет исключение класса Er;
-try {
-  $a = new Avrelii();
-}
-catch(FileEr $e) {
-  $p2 = new FileEr();
-  $p2->hand3($e);
-  //throw new FileEr('Передадим в класс FileEr');
-}
-catch(DataEr $e) {
-  $p3 = new DataEr();
-  $p3->hand();
-  throw new DataEr('Передадим в класс DataEr');
-}
-catch(Exception $e) {
-  echo "<br>" . 'Исключения базового класса Exception';
-}
 $p1 = new Er();
 $p1->hand();
-//Выскочит нефатальная ошибка;
-//include 'D:\test.php';
-// Выскочит фатальная ошибка;
-//require 'D:\test.php';
+try {
+  try {
+    $a = new Avrelii();
+  }
+  catch (FileEr $e) {
+    $p2 = new FileEr();
+    // Получили и передали направленно в метод для работы с исключениями;
+    $p2->hand3($e);
+    //throw new FileEr('Передадим в класс FileEr');
+  }
+  catch (DataEr $e) {
+    $p3 = new DataEr();
+    $p3->hand3($e);
+  }
+  catch (Exception $e) {
+    echo "<br>" . 'Исключения базового класса Exception';
+  } 
+  finally {
+    echo "<br>" . 'Текст после finally';
+  //Выскочит нефатальная ошибка;
+  //include 'D:\test.php';
+  // Выскочит фатальная ошибка;
+  //require 'D:\test.php';
+  }
+}
+catch (Exception $e) {
+  echo "<br>" . 'Необработанное исключение';
+}

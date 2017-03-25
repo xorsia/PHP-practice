e40d659c1411277063aeefc79bea8739e4adba6d
Aleksandr-Borodin <aleksandrov477@gmail.com>
2017-03-25 14:02:11 +0200

    Использован parenе, позднее связывание

--- Classes/DataEr.php
+++ Classes/DataEr.php
@@ -1,7 +1,13 @@
 <?php
 // Дочерний класс - наследник Er (Extension);
 class DataEr extends Er {
-  public function hand3(\Exception $e) {
-    echo 'Ошибкой занимается класс 2' . get_class($e);
+  public  function hand3(\Exception $e) {
+    // Сначала будет использован метод hand3 класса Er;
+    parent::hand3($e);
+    // А уже потом измененный метод hand3 дочернего класса;
+    echo "<br>" . 'Использован родительский метод + дочерний класса ' . get_class($this);
+  }
+  public function hand4() {
+    return 2;
   }
 }

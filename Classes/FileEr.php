98f1a2b2e24d177fb5e54155f36811d410441222
Aleksandr-Borodin <aleksandrov477@gmail.com>
2017-03-25 14:04:13 +0200

    Для доступа к родительскому методу использован parent, классы FileEr и DataEr имеют свои уникальные методы hand4

--- Classes/FileEr.php
+++ Classes/FileEr.php
@@ -1,7 +1,13 @@
 <?php
 // Дочерний класс - наследник Er (Extension);
 class FileEr extends Er {
-  public function hand3(\Exception $e) {
-    echo 'Ошибкой занимается класс 1' . get_class($e);
+  public  function hand3(\Exception $e) {
+    // Сначала будет использован метод hand3 класса Er;
+    parent::hand3($e);
+    // А уже потом измененный метод hand3 дочернего класса;
+    echo "<br>" . 'Использован родительский метод + дочерний класса ' . get_class($this);
+  }
+  public function hand4() {
+    return 1;
   }
 }

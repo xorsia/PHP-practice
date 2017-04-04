96408519840223349402955cbbc49fe1bb4c78de
Aleksandr-Borodin <aleksandrov477@gmail.com>
2017-04-04 15:52:03 +0300

    Транзация

--- PDO.php
+++ PDO.php
@@ -25,33 +25,30 @@ while($row = $sth->fetch()) {
 var_dump($m[0]['Название товара']);
 
 */
-// Запрос с последовательной подстановкой;
-$sql = 'SELECT * FROM tabl1 WHERE Name=:t1 AND Price=:t2';
-$sth = $dbh->prepare($sql);
-$a = 'Монитор';
-$b = 500;
-//$sth->bindValue(1, $a, PDO::PARAM_STR);
-$sth->bindValue(':t1', $a, PDO::PARAM_STR);
-$sth->bindValue(':t2', $b, PDO::PARAM_INT);
-$sth->execute();
-while ($row = $sth->fetch(PDO::FETCH_NUM)) {
-  var_dump($row);
-}
 
 
-/*
+
+
 // Трансакции;
 // Изменений в БД не будет, пока не будет завершена трансакция;
 try {
   $dbh->beginTransaction();
   $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  
-  // выполнение запроса;
-  // выполнение запроса;  
-  $dbh->comit();  // трансакция выполнена успешно;  
+  // Запрос с последовательной подстановкой;
+  $sql = 'SELECT * FROM tabl1 WHERE Name=:t1 AND Price=:t2';
+  $sth = $dbh->prepare($sql);
+  $a = 'Монитор';
+  $b = 500;
+//$sth->bindValue(1, $a, PDO::PARAM_STR);
+  $sth->bindValue(':t1', $a, PDO::PARAM_STR);
+  $sth->bindValue(':t2', $b, PDO::PARAM_INT);
+  $sth->execute();
+  while ($row = $sth->fetch(PDO::FETCH_NUM)) {
+    var_dump($row);
+  }
+  $dbh->commit();  // трансакция выполнена успешно;
 }
 catch(PDOException $e) {
   echo 'Ошибка' . $e->getMessage();
   $dbh->rollBack();  // откат трансакции;
 }
-*/

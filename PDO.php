327e666298299377d61b4a826d711e408c52e54c
Aleksandr-Borodin <aleksandrov477@gmail.com>
2017-04-04 17:07:35 +0300

    Удаление записи посредством PDO

--- PDO.php
+++ PDO.php
@@ -8,12 +8,10 @@ try {
 catch(PDOException $e) {
   echo 'Ошибка подключения: '. $e->getMessage();
 }
-
 // экранирование, защита от ошибок;
 $id = 1;
 $id = $dbh->quote($id);
 // теперь $id будет с кавычками;
-
 /*
 // Простой запрос;
 $sql = 'SELECT * FROM tabl1';
@@ -23,21 +21,39 @@ while($row = $sth->fetch()) {
   $m[] = $row;
 }
 var_dump($m[0]['Название товара']);
-
 */
-
-$allowed = ['Name', 'About', 'Price'];
-$values = ['Ноутбук', 'Средней категории', '900'];
+/*
+// Вставка новой строки в таблицу базы данных;
 $sql = "INSERT INTO tabl1 (Name, About, Price) VALUES ('Ноутбук','Средней категории','900')";
 $stm = $dbh->prepare($sql);
+// Результат выполенния будет записан в $a;
+$a = $stm->execute();
+echo "<br>" . $a;
+*/
+/*
+// Изменение строки;
+$sql = "UPDATE tabl1 SET Name='Лэптоп', About='Мини-ноутбук', Price='200' WHERE id='8'";
+$stm = $dbh->prepare($sql);
+// Результат выполенния будет записан в $a;
+$a = $stm->execute();
+echo $a;
+*/
+/*
+// Удаление строки;
+$sql = 'DELETE FROM tabl1 WHERE id=:id';
+$stm = $dbh->prepare($sql);
+$b = 9;
+$stm->bindValue(':id', $b, PDO::PARAM_INT);
+// Результат выполенния будет записан в $a;
 $stm->execute();
-
+*/
 /*
 // Трансакции;
 // Изменений в БД не будет, пока не будет завершена трансакция;
 try {
   $dbh->beginTransaction();
   $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
   // Запрос с последовательной подстановкой;
   $sql = 'SELECT * FROM tabl1 WHERE Name=:t1 AND Price=:t2';
   $sth = $dbh->prepare($sql);
@@ -50,7 +66,9 @@ try {
   while ($row = $sth->fetch(PDO::FETCH_NUM)) {
     var_dump($row);
   }
+
   $dbh->commit();  // трансакция выполнена успешно;
+
   echo "<br>" . $sth->rowcount() . "<br>";
 }
 catch(PDOException $e) {

fc1deb09666a68e2119a787bea04a41f81f8ebf4
Aleksandr-Borodin <aleksandrov477@gmail.com>
2017-05-04 17:06:24 +0300

    Картинка для работы

--- img/img.php
+++ img/img.php
@@ -2,9 +2,13 @@
 if(is_uploaded_file($_FILES['image']['tmp_name'])) {
   echo 'Пришла картинка.';
   $file_name = $_FILES['image']['tmp_name'];
+  // Информация о картинке;
   $a = getimagesize($file_name);
-
-  echo $a[3];
+  // Информация о картинке из строки;
+  $img = __DIR__ . '/paint.jpg';
+  $data = file_get_contents($img);
+  $a = getimagesizefromstring($data);
+  var_dump($a);
 } else {
   echo 'Ничего нет!';
   exit;
